67f0a68a6e0813f892e21e00bfe3fba4b05cbab8
airman5573 <rpf5573@kakao.com>
2022-06-23 15:05:01 +0900

    refactor: type을 수정한다
    0도 0px대신 사용할 수 있다

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,6 @@
 type AtLeast<T, K extends keyof T> = Partial<T> & Pick<T, K>
 
-type CssPixel = `${number}px`
+type CssPixel = 0 | '0' | `${number}px`
 
 type CssTime = 0 | '0' | `${number}s` | `${number}ms`
 
